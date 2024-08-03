ca91570f8268f2e7526f1161ac9ccd8b54e8f029
LS7710 <luissoto7710@gmail.com>
2024-08-02 20:55:53 -0400

    fixes

--- 03-frontend/static/scripts.js
+++ 03-frontend/static/scripts.js
@@ -1,8 +1,3 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
-
 document.addEventListener('DOMContentLoaded', () => {
   /* DO SOMETHING */
 });
